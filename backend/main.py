f83374f7d5d4c2d66e1f26402487091732bef75d
Miguel Angel <miguelangelgarb@gmail.com>
2025-05-05 13:08:08 -0500

    Apliacación Login

---
+++ backend/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["GET","POST"],
+    allow_headers=["*"],
+)
+
+@app.post("/login")
+async def login(request: Request):
+    data = await request.json()
+    print("Datos recibidos:", data)
+    return {"status": "ok", "username": data.get("username")}
+
--- login/src/App.js
+++ login/src/App.js
@@ -1,18 +1,31 @@
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
 
-  const handSudmit = (e) => {
+  const handSudmit = async (e) => {
     e.preventDefault();
-    console.log("Formulario enviado")
-  };
+
+  const response = await fetch("http://localhost:8000/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ email, pwd})
+  });
+
+  const data = await response.json();
+  console.log("Respuesta del backend:", data);
+};
+
   return (
     <div className="App">
       <div className="contenedor-login">
         <img src="/imagen.png" alt="Logo" className="imagen"/>
         <h1 className='encabezado'>Login</h1>
       </div>
-        
       <form onSubmit={handSudmit}>
         <label for="email">Email</label>
         <input type="text" id="email" name="email" required></input>
