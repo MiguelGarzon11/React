98109e6a5cd477ad41084a6422d35028e500295d
Miguel Angel <miguelangelgarb@gmail.com>
2025-05-23 13:10:28 -0500

    Routers React

---
+++ Routers/reactRouter.jsx
@@ -0,0 +1,12 @@
+// React Router es una biblioteca que permite crear rutas en aplicaciones React, es decir, define qué componente se muestra dependiendo de la URL, Esto es esencial para contruir Single Page Applications (SPA), donde no hay carga completa del navegador al cambiar de "página".
+
+// ¿Para qué sirve?
+// Mostrar diferentes componentes según la URL (/home, /about, /contact)
+// Navegar entre páginas sin recargar el navegador
+// Pasar datos por la URL
+// Crear rutas protegidas (como login)
+
+// Instalación:
+// Primero, debes instalarlo en tu proyecto con:
+
+// npm install react-router-dom
