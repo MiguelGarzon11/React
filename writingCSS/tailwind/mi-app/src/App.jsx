7870588a0103fb03d88a8cc4ad929a1eb7169153
Miguel Angel <miguelangelgarb@gmail.com>
2025-05-28 18:23:39 -0500

    Tailwind

---
+++ writingCSS/tailwind/mi-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  return (
+    <div className="min-h-screen bg-yellow-200 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-800">
+        ¡Tailwind está funcionando!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
